0cf1e97ad33b0a5ed4b17728d090ac7112faa737
gokhantemel <gokhan.96011@gmail.com>
2023-06-11 12:23:44 +0300

    second pr

--- src/test/java/xfleet/Gokhantest1.java
+++ src/test/java/xfleet/Gokhantest1.java
@@ -3,5 +3,6 @@ package xfleet;
 public class Gokhantest1 {
     public static void main(String[] args) {
         System.out.println("Sezeri sevenler öldü");
+        System.out.println("Sezer");
     }
 }
