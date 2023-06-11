36e89a402748bb75eaaa7868af76ff4447dd666f
recep <recepdemir1204@gmail.com>
2023-06-11 12:30:02 +0300

    recep ad71

--- src/test/java/xfleet/xfleet.java
+++ src/test/java/xfleet/xfleet.java
@@ -3,6 +3,7 @@ package xfleet;
 public class xfleet {
     public static void main(String[] args) {
         System.out.println("1-sezer seni sevmeyen ölsün!!!!");
+        System.out.println("2-sezer seni sevmeyen ölsün!!!!");
 
 
 
